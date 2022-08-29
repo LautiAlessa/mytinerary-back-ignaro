57d1c552819d18b7a8001df113e057f4b911a507
ignaciogiusti <nachogiusti94@gmail.com>
2022-08-29 13:14:40 -0300

    created databse.js

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose.connect( //depende de 2 parametros: 1ero link de conexion, 2do objeto con propiedades true
+    process.env.MONGO_CONNECTION,
+    {
+        useUnifiedTopology: true, // Habilita a mongoose a controlar la DB de mongo
+        useNewUrlParser: true // Utiliza el analizador de errores de mongoose en lugar de mongo
+    }
+)
+    .then(() => console.log('connected to databse succesfully'))
+    .catch(error => console.log(error));
