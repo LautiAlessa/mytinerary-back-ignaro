f8311233594ab2bef5fab567dbf860de978710d2
ignaciogiusti <nachogiusti94@gmail.com>
2022-08-30 23:04:44 -0300

    added missing await for let city

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -1,3 +1,4 @@
+const { query } = require('express')
 const { response } = require('../app')
 const City = require('../models/City')
 
@@ -6,33 +7,56 @@ const cityController = {
     create: async (req, res) => {
         const { city, country, photo, population, fundation } = req.body
         try {
-            await new City({ city, country, photo, population, fundation }).save()
+            await new City(req.body).save()
             res.status(201).json({
-                message: 'city created',
+                message: 'city successfully created',
                 success: true
             })
         } catch (error) {
             res.status(400).json({
-                message: "couldn't created city",
+                message: "couldn't create city",
                 success: false
             })
         }
     },
+
+    all: async (req, res) => {
+        let cities
+
+        let query = {}
+        if (req.query.country) {
+            query.capacity = req.query.country
+        }
+        if (req.query.city) {
+            query.city = req.query.city
+        }
+
+
+        try {
+            cities = await City.find()
+            res.json(cities)
+            
+        } catch (error) {
+            console.log(error)
+            res.status(500).json()
+        }
+    },
+
     read: async (req, res) => {
         const { id } = req.params
         try {
-            let city = City.findOne({ _id: id })
+            let city = await City.findOne({_id:id})
             // si city existe retrono un json con los datos
             // si city no existe -> city = {} retorno un json con 404
             if (city) {
                 res.status(200).json({
-                    message: "you get one city",
+                    message: "you got one city",
                     response: city,
                     success: true
                 })
             } else {
                 res.status(404).json({
-                    message: "couldn't find a city",
+                    message: "couldn't find that city",
                     success: false
                 })
             }
@@ -43,7 +67,23 @@ const cityController = {
                 success: false
             })
         }
-    }
+    },
+
+    update: async (req, res) => {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    destroy: async (req, res) => {
+        try {
+
+        } catch (error) {
+
+        }
+    },
 }
 
-module.export = cityController
+module.exports = cityController
