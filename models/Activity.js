af04d2238f8206dd5ed85b31fbd6ef22cfbfa24f
ignaciogiusti <nachogiusti94@gmail.com>
2022-09-07 17:55:57 -0300

    Modified models to use references

--- models/Activity.js
+++ models/Activity.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const activitySchema = new mongoose.Schema({
     name: { type: String, required: true },
     photo: { type: String, required: true},
-    itinerary: { type: String}
+    itinerary: { type: mongoose.Types.ObjectId, ref: 'itineraries' }
 })
 
 let Activity = mongoose.model(
--- models/Comment.js
+++ models/Comment.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 const commentSchema = new mongoose.Schema({
     comment: { type: String, required: true },
-    user: { type: String},
-    itinerary: { type: String}
+    user: { type: mongoose.Types.ObjectId, ref: 'users' },
+    itinerary: { type: mongoose.Types.ObjectId, ref: 'itineraries' }
 })
 
 let Comment = mongoose.model(
