f242c8ca2f76f25dff48ca19508e5cfe3320a56f
ignaciogiusti <nachogiusti94@gmail.com>
2022-09-08 01:26:57 -0300

    Created commentrController.js and comments.js for routes, inserted routes into index.js

---
+++ controllers/commentController.js
@@ -0,0 +1,79 @@
+const Comment = require('../models/Comment')
+
+const commentController = {
+    create: async (req, res) => {
+        // const { name, lastName, photo, mail, password, country } = req.body
+        try {
+            let comment = await new Comment(req.body).save()
+            res.status(201).json({
+                message: 'comment successfully created',
+                success: true
+            })
+            if (comment) {
+
+            } else {
+                res.status(406).json({
+                    message: 'cant create, comment values are invalid',
+                    success: false
+                })
+            }
+        } catch (error) {
+            res.status(400).json({
+                message: "couldn't create comment",
+                success: false
+            })
+        }
+    },
+
+    all: async (req, res) => {
+        let activities
+
+        let query = {}
+        if (req.query.comment) {
+            query.comment = req.query.comment
+        }
+        if (req.query._id) {
+            query._id = req.query._id
+        }
+        if (req.query.itinerary) {
+            query.itinerary = req.query.itinerary
+        }
+
+        try {
+            activities = await Comment.find(query)
+            .populate("itinerary", {name:1})
+            res.json({ success: true, response: activities })
+        } catch (error) {
+            console.log(error)
+            res.status(500).json()
+        }
+    },
+
+    destroy: async (req, res) => {
+        const { id } = req.params
+        let comment
+        try {
+            comment = await Comment.findOneAndDelete({ _id: id })
+            if (comment) {
+                res.status(200).json({
+                    message: 'comment removed',
+                    response: comment,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: 'comment not found, couldnt remove',
+                    success: false
+                })
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({
+                message: "error",
+                success: false
+            })
+        }
+    },
+}
+
+module.exports = commentController
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const cityRouter = require('./cities')
 const userRouter = require('./users')
 const itineraryRouter = require('./itineraries')
 const activityRouter = require('./activities')
+const commentRouter = require ('./comments')
 
 
 /* GET home page. */
@@ -15,5 +16,6 @@ router.use('/cities', cityRouter)
 router.use('/itineraries', itineraryRouter)
 router.use('/users', userRouter)
 router.use('/activities', activityRouter)
+router.use('/comments', commentRouter)
 
 module.exports = router;
